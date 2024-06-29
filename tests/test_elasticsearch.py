88b8ecf68b555087f676307b63d7470f4f8fa425
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2024-06-29 17:52:16 -0500

    Update

--- tests/test_elasticsearch.py
+++ tests/test_elasticsearch.py
@@ -5,7 +5,7 @@ import json
 
 
 # @pytest.fixture(scope="session")
-def test_elasticsearch(mock_es_client):
+def test_elasticsearch_create_index_index(mock_es_client):
     # ES 7 will be on 9200 and setup basic index with some datas into local elasticsearch cluster
     assert mock_es_client is not None
 
